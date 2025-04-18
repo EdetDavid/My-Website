bf871e7a3dcd0901337ead6fb548f76fa220789a
Dvooskid <dvooskid1234@gmail.com>
2025-04-18 18:22:14 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/home";
 import About from "./pages/about/About";
 import Projects from "./pages/projects/projects";
+import musicFile from "./assets/music/mozart.mp3";
+import BackgroundMusic from "./components/music/BackgroundMusic";
 import NotFound from "./pages/notfound/NotFound";
 import AOS from "aos";
 import Footer from "./components/footer";
@@ -39,6 +41,7 @@ const App = () => {
         ) : (
           <>
             <Navbar />
+            <BackgroundMusic src={musicFile} />
             <Routes>
               <Route exact path="/" element={<Home />} />
               <Route exact path="/about" element={<About />} />
