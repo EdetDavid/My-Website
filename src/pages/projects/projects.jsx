abc64aa046742449d938781eb11bc766cfac88d0
Dvooskid <dvooskid1234@gmail.com>
2025-11-10 08:43:01 +0100

    deleted animation.js in projects directory

--- src/pages/projects/projects.jsx
+++ src/pages/projects/projects.jsx
@@ -288,10 +288,6 @@ const ProjectCard = ({ project }) => {
   );
 };
 
-const sectionVariants = {
-  hidden: { opacity: 0, y: 40 },
-  visible: { opacity: 1, y: 0, transition: { duration: 0.7, ease: "easeOut" } },
-};
 
 const Projects = () => {
   const [searchTerm, setSearchTerm] = useState("");
